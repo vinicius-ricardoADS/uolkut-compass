5936b7623675058156ec52101e59f37ef12fe273
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-08-22 08:41:16 -0300

    feat: added component footer responsive

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,11 @@
+import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 
 const Home = () => {
     return (
         <>
             <Header home={true}/>
+            <Footer />
         </>
     )
 };
