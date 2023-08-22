f1f301f31da785e8084b8c57c5459ac28584f3bd
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-08-22 11:02:07 -0300

    feat: validation form component completed

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -10,14 +10,30 @@ const Form = () => {
 
       const [form, setForm] = useState({
         email: '',
+        emailRegister: '',
+        passwordRegister: '',
         password: '',
         ckPassword: '',
+        date_birth: '',
+        profession: '',
+        country: '',
+        city: '',
+        selected: 'Relacionamento'
     });
+
+    const [isRegister, setIsRegister] = useState(false);
 
     const [errors, setErrors] = useState({
         invalidEmail: false,
+        invalidEmailRegister: false,
         invalidFormatEmail: false,
         invalidPassword: false,
+        invalidPasswordRegister: false,
+        invalidDateRegister: false,
+        invalidProfession: false,
+        invalidCountry: false,
+        invalidCity: false,
+        invalidRelationship: false,
     })
 
     
@@ -26,17 +42,55 @@ const Form = () => {
         const fieldValue = type === 'checkbox' ? checked : value;
 
         if (type === 'text') {
-            setErrors((prevFormErros) => ({
-                ...prevFormErros,
-                invalidEmail: false,
-                invalidFormatEmail: false,
-            }))
+            if (name === 'emailRegister') {
+                setErrors((prevFormErros) => ({
+                    ...prevFormErros,
+                    invalidFormatEmail: false,
+                    invalidEmailRegister: false,
+                }));
+            }
+            
+            if (name === 'profession') {
+                setErrors((prevFormErros) => ({
+                    ...prevFormErros,
+                    invalidProfession: false,
+                }));
+            }
+            
+            if (name === 'country') {
+                setErrors((prevFormErros) => ({
+                    ...prevFormErros,
+                    invalidCountry: false,
+                }));
+            }
+
+            if (name === 'city') {
+                setErrors((prevFormErros) => ({
+                    ...prevFormErros,
+                    invalidCity: false,
+                }));
+            }
         }
 
         if (type === 'password') {
             setErrors((prevFormErros) => ({
                 ...prevFormErros,
                 invalidPassword: false,
+                invalidPasswordRegister: false
+            }))
+        }
+
+        if (type === 'date') {
+            setErrors((prevFormErros) => ({
+                ...prevFormErros,
+                invalidDateRegister: false
+            }))
+        }
+
+        if (form.selected !== 'Relacionamento') {
+            setErrors((prevFormErros) => ({
+                ...prevFormErros,
+                invalidRelationship: false,
             }))
         }
 
@@ -53,15 +107,68 @@ const Form = () => {
         } else {
             return false;
         }
-      }
+    }
+
+    const validateFormatEmailRegister = () => {
+        const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+        if (!emailRegex.test(form.emailRegister)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
     const submitHandler = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        console.log(form);
+        if (isRegister) {
+            if (form.emailRegister.trim() === '' && form.passwordRegister.trim() === '' && form.date_birth.trim() === ''
+            && form.profession.trim() === '' && form.country.trim() === '' && form.city.trim() === '' &&
+                form.selected === 'Relacionamento') {
+                setErrors({
+                    invalidEmail: false,
+                    invalidEmailRegister: true,
+                    invalidPassword: false,
+                    invalidFormatEmail: false,
+                    invalidPasswordRegister: true,
+                    invalidDateRegister: true,
+                    invalidProfession: true,
+                    invalidCountry: true,
+                    invalidCity: true,
+                    invalidRelationship: true,
+                });
+                return;
+            }
+
+            if (validateFormatEmailRegister()) {
+                setErrors({
+                    invalidEmail: false,
+                    invalidEmailRegister: false,
+                    invalidPassword: false,
+                    invalidFormatEmail: true,
+                    invalidPasswordRegister: false,
+                    invalidDateRegister: false,
+                    invalidProfession: false,
+                    invalidCountry: false,
+                    invalidCity: false,
+                    invalidRelationship: false,
+                });
+                return;
+            }
+        }
+        
         if (form.email.trim() === '' && form.password.trim() === '') {
             setErrors({
                 invalidEmail: true,
+                invalidEmailRegister: false,
                 invalidPassword: true,
                 invalidFormatEmail: false,
+                invalidPasswordRegister: false,
+                invalidDateRegister: false,
+                invalidProfession: false,
+                invalidCountry: false,
+                invalidCity: false,
+                invalidRelationship: false,
             });
             return;
         }
@@ -69,8 +176,15 @@ const Form = () => {
         if (validateFormatEmail()) {
             setErrors({
                 invalidEmail: false,
+                invalidEmailRegister: false,
                 invalidPassword: false,
                 invalidFormatEmail: true,
+                invalidPasswordRegister: false,
+                invalidDateRegister: false,
+                invalidProfession: false,
+                invalidCountry: false,
+                invalidCity: false,
+                invalidRelationship: false,
             });
             return;
         }
@@ -80,58 +194,135 @@ const Form = () => {
 
     return (
         <>
-        <div className={classes['container-form']}>
-                <div className={classes['brand-form']}>
-                    <img src={orkut} className={classes['img-orkut']} alt='Brand Orkut' />
-                    <h3 className={classes.title}>Acesse o Orkut</h3>
+            {isRegister ? (
+                <div className={classes['container-form']}>
+                    <div className={classes['brand-form']}>
+                            <img src={orkut} className={classes['img-orkut']} alt='Brand Orkut' />
+                            <h3 className={classes.title}>Cadastre-se no UOLkut</h3>
+                    </div>
+                    <form className={classes.form} onSubmit={submitHandler}>
+                        <div className={classes['input-container']}>
+                                <div className={classes['label-float']}>
+                                    <input onChange={handleChange} name='emailRegister'
+                                        value={form.emailRegister} className={errors.invalidEmailRegister ? classes['invalid-input'] : classes['input-register']} 
+                                        type="text" placeholder="E-mail"/>
+                                </div>
+                            </div>
+                            <div className={classes['input-container']}>
+                                <div className={classes['label-float']}>
+                                    <input onChange={handleChange} name='passwordRegister' 
+                                        value={form.passwordRegister} className={errors.invalidPasswordRegister ? classes['invalid-input'] : classes['input-register']}
+                                        type="password" placeholder="Senha"/>
+                                </div>
+                            </div>
+                            <div className={classes['input-container-flex']}>
+                                <div>
+                                    <div className={classes['label-float']}>
+                                        <input onChange={handleChange} name='date_birth' 
+                                            value={form.date_birth} className={errors.invalidDateRegister ? classes['invalid-input-flex'] : classes['input-flex']}
+                                            type="date" placeholder="Nascimento"/>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className={classes['label-float']}>
+                                        <input onChange={handleChange} name='profession' 
+                                            value={form.profession} className={errors.invalidProfession ? classes['invalid-input-flex'] : classes['input-flex']}
+                                            type="text" placeholder="Profissão"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={classes['input-container-flex']}>
+                                <div>
+                                    <div className={classes['label-float']}>
+                                        <input onChange={handleChange} name='country' 
+                                            value={form.country} className={errors.invalidCountry ? classes['invalid-input-flex'] : classes['input-flex']}
+                                            type="text" placeholder="País"/>
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className={classes['label-float']}>
+                                        <input onChange={handleChange} name='city' 
+                                            value={form.city} className={errors.invalidCity ? classes['invalid-input-flex'] : classes['input-flex']}
+                                            type="text" placeholder="Cidade"/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={classes['input-container']}>
+                                <div className={classes['label-float']}>
+                                    <select className={errors.invalidRelationship ? classes['invalid-input-select'] : classes['input-select']} value={form.selected} onChange={e => setForm((prevState) => ({
+                                        ...prevState,
+                                        selected: e.target.value
+                                    }))} name="relationship">
+                                        <option value="Relacionamento">Relacionamento</option>
+                                        <option value="Solteiro">Solteiro</option>
+                                        <option value="Casado">Casado</option>
+                                        <option value="Viúvo">Viúvo</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className={classes['flex-button-register']}>
+                                <button className={classes['btn-signup']}>
+                                    <span className={classes['signup-description']}>
+                                        Criar uma conta
+                                    </span>
+                                </button>
+                            </div>
+                    </form>
                 </div>
-                <form className={classes.form} onSubmit={submitHandler}>
-                <div className={classes['input-container']}>
-                        <div className={classes['label-float']}>
-                            <input onChange={handleChange} name='email'
-                                value={form.email} className={errors.invalidEmail ? classes['invalid-input'] : classes.input} 
-                                type="text" placeholder="E-mail"/>
-                        </div>
-                        {errors.invalidEmail &&
-                            <p className={classes.errors}>Campo de email não pode estar vazio</p>
-                        }
-                        {errors.invalidFormatEmail &&
-                            <p className={classes.errors}>Campo de email não é válido</p>
-                        }
+            ) : (
+                <div className={classes['container-form']}>
+                    <div className={classes['brand-form']}>
+                            <img src={orkut} className={classes['img-orkut']} alt='Brand Orkut' />
+                            <h3 className={classes.title}>Acesse o UOLkut</h3>
                     </div>
-                    <div className={classes['input-container']}>
-                        <div className={classes['label-float']}>
-                            <input onChange={handleChange} name='password' 
-                                value={form.password} className={errors.invalidPassword ? classes['invalid-input'] : classes.input}
-                                 type="password" placeholder="Senha"/>
-                        </div>
-                        {errors.invalidPassword &&
-                            <p className={classes.errors}>Campo de senha não pode estar vazio</p>
-                        }
-                    </div>
-                    <div className={classes['password-flex']}>
-                        <label htmlFor="ckbox" className={classes.checkbox}>
-                            Lembrar minha senha
-                            <input onChange={handleChange} type="checkbox" name="ckPassword" 
-                                id="ckbox" value={form.ckPassword} />
-                            <span className={classes.checkmarkPassword}></span>
-                        </label>
-                    </div>
-                    <div className={classes['flex-button']}>
-                        <button className={classes['btn-signin']} >
-                            <span className={classes['signin-description']}>
-                                Entrar na conta
-                            </span>
-                        </button>
-                        <button className={classes['btn-signup']}>
-                            <span className={classes['signup-description']}>
-                                Criar uma conta
-                            </span>
-                        </button>
-                        <a className={classes['anchor-password']} href='/'>Esqueci minha senha</a>
-                    </div>
-                </form>
-            </div>
+                    <form className={classes.form} onSubmit={submitHandler}>
+                        <div className={classes['input-container']}>
+                                <div className={classes['label-float']}>
+                                    <input onChange={handleChange} name='email'
+                                        value={form.email} className={errors.invalidEmail ? classes['invalid-input'] : classes.input} 
+                                        type="text" placeholder="E-mail"/>
+                                </div>
+                                {errors.invalidEmail &&
+                                    <p className={classes.errors}>Campo de email não pode estar vazio</p>
+                                }
+                                {errors.invalidFormatEmail &&
+                                    <p className={classes.errors}>Campo de email não é válido</p>
+                                }
+                            </div>
+                            <div className={classes['input-container']}>
+                                <div className={classes['label-float']}>
+                                    <input onChange={handleChange} name='password' 
+                                        value={form.password} className={errors.invalidPassword ? classes['invalid-input'] : classes.input}
+                                        type="password" placeholder="Senha"/>
+                                </div>
+                                {errors.invalidPassword &&
+                                    <p className={classes.errors}>Campo de senha não pode estar vazio</p>
+                                }
+                            </div>
+                            <div className={classes['password-flex']}>
+                                <label htmlFor="ckbox" className={classes.checkbox}>
+                                    Lembrar minha senha
+                                    <input onChange={handleChange} type="checkbox" name="ckPassword" 
+                                        id="ckbox" value={form.ckPassword} />
+                                    <span className={classes.checkmarkPassword}></span>
+                                </label>
+                            </div>
+                            <div className={classes['flex-button']}>
+                                <button className={classes['btn-signin']} onClick={() => setIsRegister(false)}>
+                                    <span className={classes['signin-description']}>
+                                        Entrar na conta
+                                    </span>
+                                </button>
+                                <button className={classes['btn-signup']} onClick={() => setIsRegister(true)}>
+                                    <span className={classes['signup-description']}>
+                                        Criar uma conta
+                                    </span>
+                                </button>
+                                <a className={classes['anchor-password']} href='/'>Esqueci minha senha</a>
+                            </div>
+                    </form>
+                </div>
+            )}
         </>
     )
 };
