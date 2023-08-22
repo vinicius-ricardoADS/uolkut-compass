192cf20ced69a1c11d14d9390f9f83e5dbcdef9e
Vinicius Noli <viniciusnoli2016@gmail.com>
2023-08-22 14:18:42 -0300

    feat: created type for model User

---
+++ src/services/api.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { User } from "../types/User";
+
+const baseUrl = "http://localhost:5173/api/users";
+
+export const post = async (user: User) => {
+    try {
+        const response = await axios.post(baseUrl, user);
+        return response.data;
+    } catch (error) {
+        return {
+            message: error
+        }
+    }
+};
+
+export const put = async (userId: number, user: User) => {
+    try {
+        const response = await axios.put(`${baseUrl}/${userId}`, user);
+        return response.data;
+    } catch (error) {
+        return {
+            message: error
+        }
+    }
+};
---
+++ src/types/User.ts
@@ -0,0 +1,10 @@
+export interface User {
+    id: number;
+    email: string;
+    password: string;
+    date_birth: Date;
+    profession: string;
+    country: string;
+    city: string;
+    relationship: string;
+}
